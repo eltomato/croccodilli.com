7702b81a0f9cf64cfa9efc0269439554c6d2d820
eltomato <t.previero@gmail.com>
2016-02-13 12:05:29 +0100

    Iniziata modifica mobile

--- js/croccodilli.js
+++ js/croccodilli.js
@@ -8,6 +8,26 @@ croccodilli.controller('SiteController', ['$scope', '$interval', function($scope
 	$scope.minuteInMillis = 60*1000;
 	$scope.secondInMillis = 1000;
 
+	$scope.phrases = [{
+		caption: 'Non preoccuparti se il mondo finir&agrave; oggi. &Egrave; gi&agrave; domani in Australia.',
+		who: 'Charles M. Schulz'
+	}, {
+		caption: 'Le persone non fanno i viaggi, sono i viaggi che fanno le persone.',
+		who: 'Jhon Steinbeck'
+	}, {
+		caption: 'La cosa pi&ugrave; pericolosa da fare &egrave; rimanere immobili.',
+		who: 'William Burroughs'
+	}, {
+		caption: 'Dobbiamo andare e non fermarci finch&egrave; non siamo arrivati. Dove andiamo? Non lo so ma dobbiamo andare.',
+		who: 'Jack Kerouac'
+	}, {
+		caption: 'Non smetteremo di eplorare. <br>Ed alla fine di tutto il nostro andare, ritorneremo al punto di partenza per conoscerlo per la prima volta.',
+		who: 'T. S. Eliot'
+	}, {
+		caption: 'Tra vent&prime;anni sarete pi&ugrave; delusi per le cose che non avete fatto che per quelle che avete fatto. <br>Quindi mollate le cime. <br>Allontanatevi dal porto sicuro. <br>Prendete con le vostre vele i venti. <br> Eplorate. <br> Sognate. <br>Scoprite.',
+		who: 'Mark Twain'
+	}];
+
 	$scope.seconds = 0;
 
 	$scope.getDepartureMillis = function() {
