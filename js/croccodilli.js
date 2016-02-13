dbc725572f938d4d5e0b0f3c76fbf1f418a064ff
eltomato <t.previero@gmail.com>
2016-02-13 01:20:25 +0100

    Inizio lavori grafica

--- js/croccodilli.js
+++ js/croccodilli.js
@@ -1,35 +1,47 @@
-var croccodilli = angular.module('croccodilli', []);
+var croccodilli = angular.module('croccodilli', ['angular-svg-round-progress']);
 
 croccodilli.controller('SiteController', ['$scope', '$interval', function($scope, $interval) {
 	$scope.currentMillis = new Date().getTime();
-	$scope.endDate = 1472139914137;
+	$scope.endDate = 1472119200319;
 	$scope.dayInMillis = 24*60*60*1000;
 	$scope.hourInMillis = 60*60*1000;
 	$scope.minuteInMillis = 60*1000;
 	$scope.secondInMillis = 1000;
 
+	$scope.seconds = 0;
+
 	$scope.getDepartureMillis = function() {
 		return $scope.endDate - $scope.currentMillis;
 	};
 
-	$scope.days = function() {
+	$scope.calcDays = function() {
 		return Math.floor(($scope.getDepartureMillis() / $scope.dayInMillis));
 	};
 
-	$scope.hours = function() {
+	$scope.calcHours = function() {
 		return Math.floor(($scope.getDepartureMillis() % $scope.dayInMillis) / $scope.hourInMillis);
 	};
 
-	$scope.minutes = function() {
+	$scope.calcMinutes = function() {
 		return Math.floor((($scope.getDepartureMillis() % $scope.dayInMillis) % $scope.hourInMillis) / $scope.minuteInMillis);
 	};
 
-	$scope.seconds = function() {
+	$scope.calcSeconds = function() {
 		return Math.floor(((($scope.getDepartureMillis() % $scope.dayInMillis) % $scope.hourInMillis) % $scope.minuteInMillis) / $scope.secondInMillis);
 	};
 
-	$interval(function() {
+	$scope.updateTime = function() {
 		$scope.currentMillis = $scope.currentMillis + $scope.secondInMillis;
+		$scope.seconds = $scope.calcSeconds();
+		$scope.minutes = $scope.calcMinutes();
+		$scope.hours = $scope.calcHours();
+		$scope.days = $scope.calcDays();
+	};
+
+	$interval(function() {
+		$scope.updateTime();
 	}, 1000);
+
+	$scope.updateTime();
 
 }]);
