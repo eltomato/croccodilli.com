866786b3d830c7637990f50e2881628b8f03d215
eltomato <t.previero@gmail.com>
2016-02-16 19:59:59 +0100

    Inizio integrazione facebook

--- js/FacebookController.js
+++ js/FacebookController.js
@@ -16,8 +16,21 @@ croccodilli.controller('FacebookController', ['$scope', function($scope) {
 	$scope.callbackStatus = function(response) {
 		console.log(response);
 		if (response.status === 'connected') {
+			$scope.isLogged = true;
+			$scope.getFacebookData();
 		} else if (response.status === 'not_authorized') {
+			$scope.isLogged = false;
 		} else {
+			$scope.isLogged = false;
 		}
+	};
+
+	$scope.getFacebookData = function() {
+		FB.api('/me', function(response) {
+			console.log(response);
+			$scope.$apply(function() {
+				$scope.name = response.name;
+			});
+		};
 	};
 }]);
