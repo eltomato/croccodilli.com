c4974114c3c79391b00535a1a51a4739b6e6c7f3
eltomato <t.previero@gmail.com>
2016-02-20 00:12:37 +0100

    Fix pubblicazione post

--- js/PostService.js
+++ js/PostService.js
@@ -60,7 +60,7 @@ angular.module('croccodilli.services').service('postService', ['$http', '$q', fu
 				"append-to-google-spreadsheet", {
 					"file_id": file_id,
 					"worksheet_id": worksheet_id,
-					"values": [[post.poster, post.posterImageUrl, post.content]]
+					"values": [[post.identifier, post.refer, post.mail, post.poster, post.posterImageUrl, post.content]]
 				},{
 					"api_key": "br_24567_c5297836d2d26f1c73f111fff03f51a4478553e5"
 				}, function(res) {
